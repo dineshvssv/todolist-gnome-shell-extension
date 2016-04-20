cdbb0a4d52736e5f7354adedd56dfef7313e6c4c
sgnls <global@sgnls.net>
2016-04-21 00:21:24 +0100

    Commit

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-
 # Update locale
 echo "Build locale"
 cd ./locale
--- extension.js
+++ extension.js
@@ -26,7 +26,7 @@ const ExtensionSettings = Utils.getSettings(); // Get settings from utils.js
 const MAX_LENGTH = 100;
 const KEY_RETURN = 65293;
 const KEY_ENTER  = 65421;
-const BASE_TASKS = "Do something\nDo something else\nDo more stuff\nDo that again\n";
+const BASE_TASKS = "Do\nSomething\n";
 
 let todolist;	// Todolist instance
 let meta;
